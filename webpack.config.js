10f6995acdbe956f5157a65fd18e186130f18794
A.Masoud Beheshti <am.beheshti@deltaban.local>
2022-02-14 15:40:12 +0330

    add html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
+
 const config = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
 
 module.exports = config;
